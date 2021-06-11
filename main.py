7f7cfd6c99095f55a85e3c580eb4ce723907d96f
aminskey <rhshaaf@gmail.com>
2021-06-11 20:06:22 +0200

    Making code easier to understand

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
+# full imports
 import pygame, sys, random
 
+# import specific methods
 from pygame.locals import *
 from datetime import datetime
 from time import sleep, time
 from os import remove
 from warnings import filterwarnings
 
-
+# filter warning
 filterwarnings('ignore', category=DeprecationWarning)
 
+# initiate sound and graphics
 pygame.init()
-pygame.mixer.init
+pygame.mixer.init()
 
+# Create FPS handler
 clock = pygame.time.Clock()
 
+# Resolution
 width = 800
 height = 600
 
-
+# resolution tuple
 res = (width, height)
 
+# Game name
 name = 'Sky Dash'
 
+# Setting up window
 screen = pygame.display.set_mode(res)
 pygame.display.set_caption(name)
 
@@ -40,7 +47,10 @@ BG2 = (100, 100, 255)
 # Invisible mouse
 pygame.mouse.set_visible(False)
 
+# Initial Player speed
 PSD = 7
+
+# player speed
 PlayerSpeed = PSD
 
 CHANCE = 128
@@ -52,8 +62,9 @@ FPS = 110
 vec = pygame.math.Vector2
 highscore = vec(0, 0)
 
-
+# Clouds Class
 class Clouds(pygame.sprite.Sprite):
+	# General settings
 	def __init__(self, pos):
 		super().__init__()
 
@@ -62,22 +73,30 @@ class Clouds(pygame.sprite.Sprite):
 
 		self.rect.center = pos
 
+	# Make them move in the air
 	def update(self):
 		x, y = self.rect.center
 
+		# if out of window then respawn ahead of widthspan
 		if x < -self.image.get_width() - 10:
 			random.seed(datetime.now())
 			x = width + self.image.get_width() + 10
 			y = random.randrange(0, height)
 
+		# slowly drift backwards creating virtual effect
 		x-=1
 
 		self.rect.center = (x, y)
 
+# platform mechanism
 class Platform(pygame.sprite.Sprite):
+	# initial settings
+	# asking Program if landable
 	def __init__(self, Landable, image=None):
 		super().__init__()
 
+		# if there is no specific image then use random
+		# if not landable then use lava block image.
 		if image == None:
 			if Landable:
 				self.image = pygame.image.load('platform_' + str(random.randint(0, 2)) + '.png')
@@ -86,11 +105,14 @@ class Platform(pygame.sprite.Sprite):
 		else:
 			self.image = pygame.image.load(image)
 
+		# General settings
 		self.rect = self.image.get_rect()
 		self.rect.center = (random.randrange(width * 1.25, width * 1.5), random.randrange(height * 7//12, height * 5//6))
 		self.pos = vec((self.rect.center))
 
+# Seagulls class
 class Seagull(pygame.sprite.Sprite):
+	# initial settings
 	def __init__(self):
 		super().__init__()
 
@@ -101,10 +123,12 @@ class Seagull(pygame.sprite.Sprite):
 
 		self.x, self.y = self.rect.center
 
+	# Update mechanism
 	def update(self):
 		self.x -= PlayerSpeed * 1.15
 		self.rect.center = (self.x, self.y)
 
+		# if seagull out of screen then kill it
 		if self.x < 0:
 			all_sprites.remove(self)
 			danger.remove(self)
@@ -112,7 +136,10 @@ class Seagull(pygame.sprite.Sprite):
 
 			self.kill()
 
+
+# Player class
 class Player(pygame.sprite.Sprite):
+	# initial settings
 	def __init__(self):
 		super().__init__()
 
@@ -133,67 +160,65 @@ class Player(pygame.sprite.Sprite):
 
 		self.jumpstate = True
 
-	def move(self):
-
-		x, y = self.rect.center
-
-		keys = pygame.key.get_pressed()
-
-		if pygame.sprite.spritecollide(self, platforms, False):
-			plat = pygame.sprite.spritecollide(self, platforms, False)
-			px, py = plat[0].rect.topleft
-
-			if y <= py:
-				x+=PlayerSpeed
-				self.relpos.x += PlayerSpeed
-		else:
-			x+=PlayerSpeed
-			self.relpos.x += PlayerSpeed
-
-		self.rect.center = (x, y)
-
+	# Jump mechanism
 	def jump(self):
 
 		x, y = self.rect.center
 
 
+		# key mapping
 		keys = pygame.key.get_pressed()
 		mkeys = pygame.mouse.get_pressed()
 
 
+		# Event handling
 		if keys[K_SPACE] or mkeys[0]:
+
+			# Playing jump sound
 			self.sound.stop()
 			self.sound.play()
+
+			# jumping
 			y -= 20
 
+		# Updating position
 		self.rect.center = (x, y)
 
+	# Gravity mechanism
 	def gravity(self):
 
 		x, y = self.rect.center
 
+		# checking for collision
+		# If not then continue falling and updating position
 		if not pygame.sprite.spritecollide(self, platforms, False):
 			y += self.acc
 			self.acc += 0.4
 
 			self.rect.center = (x, y)
+		# else stop and update position
 		else:
 			self.acc = 0
 			self.rect.center = (x, y)
 
 
 
+	# The Update mechanism
 	def update(self):
 
 		x, y = self.rect.midtop
 
+		# Creating bounding box
 		if x > width * 2//3:
 			x = width * 2//3
 
+		# Updating Position 
 		self.rect.midtop = (x, y)
 
 
+		# Checking for collision
 		if pygame.sprite.spritecollide(self, platforms, False):
+
 			plats = pygame.sprite.spritecollide(self, platforms, False)
 			plat = plats[-1]
 			platx, platy = plat.rect.midtop
@@ -201,6 +226,8 @@ class Player(pygame.sprite.Sprite):
 			plat_w = plat.image.get_width()
 			plat_h = plat.image.get_height()
 
+			# If player collides with platform and is deep in the block
+			# Then stop all motion except for jumping mechanism
 			if y > platy and x > (platx + plat_w * 1//3) and x < (platx + plat_w * 2//3):
 				self.jumpstate = False
 				self.rect.midtop = (platx, y)
@@ -208,14 +235,17 @@ class Player(pygame.sprite.Sprite):
 				self.jumpstate = True
 
 
+		# Updating relative position
 		self.relpos.x += PlayerSpeed
 
+		# If not in air allow jump mechanism
 		if self.jumpstate:
 			self.jump()
 
+		# Running virtual gravity method
 		self.gravity()
 
-
+# ScoreLine class
 class HighScoreLine(Player):
 	def __init__(self):
 		super().__init__()
@@ -230,8 +260,10 @@ class HighScoreLine(Player):
 		self.x, self.y = self.rect.center
 
 def main():
+	# choosing random background song.
 	pygame.mixer.music.load('song-' + str(random.randint(0, 4)) +'.ogg')
 
+	# Creating background clouds
 	for i in range(40):
 		new_cloud = Clouds((random.randrange(0, width), random.randrange(0, height)))
 
@@ -239,10 +271,12 @@ def main():
 			clouds.add(new_cloud)
 			all_sprites.add(new_cloud)
 
+	# Importing global variables
 	global highscore
 	global PlayerSpeed
 	global CHANCE
 
+	# Creating scoreLine
 	scoreLine = HighScoreLine()
 	all_sprites.add(scoreLine)
 
@@ -254,43 +288,51 @@ def main():
 	# Defining ground platform
 	plat1 = Platform(True, 'platform_0.png')
 
+	# Customizing platform
 	plat1.image = pygame.transform.scale(plat1.image, (width, plat1.image.get_height()))
 	plat1.rect = plat1.image.get_rect()
 	plat1.rect.topleft = (0, height * 5//6 + 3)
 
+	# Add initial platform to groups
 	platforms.add(plat1)
 	all_sprites.add(plat1)
 
-	platx, platy = plat1.rect.center
-
+	# loop the background music
 	pygame.mixer.music.play(-1, 0)
 
 
+	# Creating font object
 	sub = pygame.font.Font('pixelart.ttf', 25)
 
 	while True:
 
+		# HUD highscore text
 		hs1 = sub.render('Current Highscore: ', BG2, (55,55,255))
 		hs2 = sub.render(str(highscore.x//100), BG2, (55, 55, 255))
 
+		# text rectangle
 		hs1Rect = hs1.get_rect()
 		hs2Rect = hs2.get_rect()
 
+		# Positioning text
 		hs1Rect.center = (width * 6//16, hs1.get_height())
 		hs2Rect.center = (width * 11//16, hs2.get_height())
 
 
 
-
+		# HUD Player text
 		score1 = sub.render('Player Score', BG2, (55,55,255))
 		score2 = sub.render(str(p1.relpos.x//100), BG2, (55,55,255))
 
+		# text rectangle
 		score1Rect = score1.get_rect()
 		score2Rect = score2.get_rect()
 
+		# Positioning text
 		score1Rect.center = (width * 6//16, score1.get_height()*2)
 		score2Rect.center = (width  * 11//16, score2.get_height()*2)
 
+		# Window event handler
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
@@ -301,15 +343,25 @@ def main():
 
 		x, y = p1.rect.center
 
+		# Making the platforms move to create an illusion
+		# That the player is moving
 		for plat in platforms:
 			px, py = plat.rect.center
 			px -= PlayerSpeed
 			plat.rect.center = (px, py)
 
+
+		# if player's x is greater than highscore bar's x
+		# Then update it
 		if x > scoreLine.x:
 			scoreLine.x -= PlayerSpeed
 
+
+		# Put in for loop for user to increase game intensity
 		for i in range(1):
+			# randomizing chance
+			# same logic as the seagull (l. 352) but with lava blocks
+			# but not dependent on distance
 			random.seed(datetime.now())
 
 			choice = random.randint(0, CHANCE)
@@ -321,17 +373,22 @@ def main():
 				new_plat = Platform(True)
 
 
+			# if platforms overlap
+			# remove them
 			if not pygame.sprite.spritecollide(new_plat, platforms, False):
 				platforms.add(new_plat)
 				all_sprites.add(new_plat)
 			else:
 				new_plat.kill()
 
+		# checking if the player died
+		# If it happened then reset settings and run gameOver method
 		if y > height * 2 or pygame.sprite.spritecollide(p1, danger, False) or x < 0:
 			PlayerSpeed = PSD
 			CHANCE = 128
 			gameOver(p1, highscore)
 
+		# if platform is out of screen or if there are more than 10 platforms then destroy
 		i = 0
 		for plat in platforms:
 			i+=1
@@ -339,9 +396,12 @@ def main():
 			if px <= 0 or i > 10:
 				plat.kill()
 
+		# Seagull will most likely spawn after player score is 300. May not happen at 300 due to possibility
 		if p1.relpos.x > 30000 == 0:
+			# Chance of a seagull spawning
 			choice = random.randint(0, CHANCE)
 
+			# If true then spawn
 			if choice == 0:
 				for i in range(random.randrange(0, 10)):
 					new_seagull = Seagull()
@@ -351,34 +411,44 @@ def main():
 						all_sprites.add(new_seagull)
 						danger.add(new_seagull)
 
+		# Checking if player's relative position greater then highscore position
+		# If yes then update
 		if p1.relpos.x >= highscore.x:
 			highscore.x = p1.relpos.x
 			scoreLine.x = x
 		else:
 			scoreLine.x = highscore.x
 
-		# When players score divided by 10 gives a remainder of 0.
+		# When players score divided by 100 gives a remainder of 0.
 		# And if player score not zero its self
 
 		if p1.relpos.x % 10000 == 0 and p1.relpos.x != 0:
 			PlayerSpeed += 1
 			CHANCE //= 4
 
+		# Updating scoreline
 		scoreLine.rect.center = (scoreLine.x, height//2)
 
+
+		# Updating sprite groups
 		clouds.update()
 		seagulls.update()
 		p1.update()
 
+		# Drawing all sprites to screen
 		all_sprites.draw(screen)
 
+		# Sending Highscore and player score data to screen
 		screen.blit(hs1, hs1Rect)
 		screen.blit(hs2, hs2Rect)
 
 		screen.blit(score1, score1Rect)
 		screen.blit(score2, score2Rect)
 
+		# Refreshing screen
 		pygame.display.update()
+
+		# Fixed Frame rate 120 recommended unless old computer
 		clock.tick(FPS)
 
 def startScreen():
